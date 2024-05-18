1fee755f33b8556b7862505234aa2dbd877f2021
Fatih <faatihboz@gmail.com>
2024-05-18 16:46:20 +0300

    SFX control

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -8,16 +8,28 @@ public class AudioManager : MonoBehaviour
 
     public bool SoundOn = true;
 
+    #region clips
+    [SerializeField] private AudioClip simpleButtonClickSF;
+    #endregion
+
+    private AudioSource audioSource;
+
     private void Awake()
     {
         if(instance == null)
         {
             instance = this;
         }
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void SimpleButtonClickSF()
     {
-
+        if(SoundOn)
+        {
+            audioSource.PlayOneShot(simpleButtonClickSF);
+        }
+        
     }
 }
