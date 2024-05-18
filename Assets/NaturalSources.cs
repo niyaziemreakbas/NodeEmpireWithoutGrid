c5d5f18212fc5d8c048fbb8fd94a4132499b0359
Fatih <faatihboz@gmail.com>
2024-05-18 23:37:18 +0300

    enemy node update

--- Assets/NaturalSources.cs
+++ Assets/NaturalSources.cs
@@ -6,7 +6,7 @@ public class NaturalSources : MonoBehaviour
 {
     [SerializeField] private SourceType sourceType;
     private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.CompareTag("PlayerNode"))
+        if (other.gameObject.CompareTag("PlayerNode") || other.gameObject.CompareTag("EnemyNode"))
         {
             other.gameObject.GetComponent<NodeResources>().SetResourceProductionSpeed(sourceType);
         }
