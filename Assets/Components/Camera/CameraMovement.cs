77d2308c994990b2c4a6e390b6eb9d70d502d52a
Fatih <faatihboz@gmail.com>
2024-05-19 15:09:14 +0300

    Map added

--- Assets/Components/Camera/CameraMovement.cs
+++ Assets/Components/Camera/CameraMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public float borderX;
+    public float borderY;
+    public float minusBorderY;
     
     [SerializeField]private Vector3 offset;
     [SerializeField]private float damping;
@@ -18,9 +21,32 @@ public class CameraMovement : MonoBehaviour
 
     void Update()
     {
+        RestrictCamera();
+        
     Vector3 movementInput=new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"),0);
     movementInput.Normalize();
     Vector3 targetPosition=transform.position+movementInput*speed;
     transform.position=Vector3.SmoothDamp(transform.position,targetPosition,ref vel,damping);        
+    }
+
+    void RestrictCamera()
+    {
+        if (transform.position.x > borderX)
+        {
+            transform.position = new Vector3(borderX,transform.position.y,transform.position.z);
+        }
+        else if(transform.position.x < borderX * -1)
+        { 
+            transform.position = new Vector3(borderX * -1, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.y > borderY)
+        {
+            transform.position = new Vector3(transform.position.x, borderY, transform.position.z);
+        }
+        else if (transform.position.y < minusBorderY)
+        {
+            transform.position = new Vector3(transform.position.x, minusBorderY, transform.position.z);
+        }
     }
 }
